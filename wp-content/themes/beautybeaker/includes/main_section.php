6a63c11656690eda720223fc7b4b2d3e09b4f256
Juphet Vitualla <jvitualla@dnamicro.com>
2021-09-14 20:11:15 +0800

    added animate

--- wp-content/themes/beautybeaker/includes/main_section.php
+++ wp-content/themes/beautybeaker/includes/main_section.php
@@ -1,21 +1,21 @@
 <h1>How It Works</h1>
-<div class="main_inner">
-    <div class="m-item">
+<div class="main_inner animatedParent animateOnce">
+    <div class="m-item animated bounceInUp">
         <img src="<?php bloginfo('template_url');?>/images/step1.jpg" alt="">
         <span>Step 1</span>
         <h2>Complete <span>Your Profile</span></h2>
     </div>
-    <div class="m-item">
+    <div class="m-item animated bounceInUp delay-500">
         <img src="<?php bloginfo('template_url');?>/images/step2.jpg" alt="">
         <span>Step 2</span>
         <h2>Use and <span>Experience Products</span></h2>
     </div>
-    <div class="m-item">
+    <div class="m-item animated bounceInUp delay-1000">
         <img src="<?php bloginfo('template_url');?>/images/step3.jpg" alt="image tree">
         <span>Step 3</span>
         <h2>Provide <span>Feedback & Review</span></h2>
     </div>
 </div>
 <div class="main_btm">
-    <a class="btn_get btn-site" href="">Get Started</a>
+    <a class="btn_get btn-site" href="<?= site_url()?>/sign-up">Get Started</a>
 </div>
