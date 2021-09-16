26e76de446b941fcf6a0571d08be1f18c52a40d6
Juphet Vitualla <jvitualla@dnamicro.com>
2021-09-16 21:50:39 +0800

    added cart

--- wp-content/themes/beautybeaker/includes/header.php
+++ wp-content/themes/beautybeaker/includes/header.php
@@ -8,17 +8,22 @@
             </header>
             <nav class='main_nav'>
                 <span class="mobileClose"><i class='fas fa-times'></i></span>
-                <ul>
-                    <li><a href="<?= site_url()?>/register-2">Sign Up</a></li>
-                     <?php  wp_nav_menu( array( 'container_class' => 'nav-menu', 'theme_location' => 'primary' )); ?> 
-                     <li>
-                         <a href="<?= site_url()?>/my-account"><i class='fas fa-user'></i> <?= is_user_logged_in() ? 'My Account' :'Login' ?></a>
-                     </li>
-                </ul>
+                <div class='nav-cart-cont'>
+                    <ul>
+                        <li><a href="<?= site_url()?>/register-2">Sign Up</a></li>
+                        <?php  wp_nav_menu( array( 'container_class' => 'nav-menu', 'theme_location' => 'primary' )); ?> 
+                        <li>
+                            <a href="<?= site_url()?>/my-account"><i class='fas fa-user'></i> <?= is_user_logged_in() ? 'My Account' :'Login' ?></a>
+                        </li>
+                    </ul>
+                    <div class='cart-cont'>
+                        <a href="<?= site_url()?>/cart"><i class='fas fa-shopping-cart'></i> <span class='cart-num'><?= do_shortcode('[cart_count]')?></span></a>
+                    </div>
+                </div>
             </nav>
             <div class="mobileBar">
-                    <a href="#"><i class='fas fa-bars'></i></a>
-                </div>
+                <a href="#"><i class='fas fa-bars'></i></a>
+            </div>
         </div>
     </div>
 </div>
